738d5f5b2a382e01eacb2383fcc5ced758f8ff1d
elephant_seal <mikan.ha.orange123@gmail.com>
2024-02-12 13:53:01 +0900

    初期セットアップ

--- Week3.java
+++ Week3.java
@@ -1,23 +1,29 @@
 import java.util.HashMap;
 import java.util.Map;
- public class Week3 {
-        public static Map<Integer, String> userById = new HashMap<>(){{
-            put(1,"田中");
-            put(2,"鈴木");
-            put(3,"山田");
-        }};
-        public static void main(String[] args) {
-            try {
-                System.out.println("登録されている名前は"+findUserById(3)+"さんです。");
-            }catch (NullPointerException e) {
-                System.out.println("登録されていません");
-            }
+
+public class Week3 {
+
+    public static Map<Integer, String> userById = new HashMap<>(){{
+        put(1,"田中");
+        put(2,"鈴木");
+        put(3,"山田");
+    }};
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("登録されている名前は"+findUserById(3)+"さんです。");
+        }catch (NullPointerException e) {
+            System.out.println("登録されていません");
         }
-        public static String findUserById(int id) throws NullPointerException{
-            String userName = userById.get(id);
-            if(userName == null) {
-                throw new NullPointerException();
-            }
-            return userName;
+    }
+
+    public static String findUserById(int id) throws NullPointerException{
+        String userName = userById.get(id);
+
+        if(userName == null) {
+            throw new NullPointerException();
         }
- }
+        return userName;
+    }
+
+}
