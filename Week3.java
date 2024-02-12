53f5da26b062917d2dcc9d0ebd52b3e0cd522215
elephant_seal <mikan.ha.orange123@gmail.com>
2024-02-12 13:35:54 +0900

    初期セットアップ

---
+++ Week3.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+ public class Week3 {
+        public static Map<Integer, String> userById = new HashMap<>(){{
+            put(1,"田中");
+            put(2,"鈴木");
+            put(3,"山田");
+        }};
+        public static void main(String[] args) {
+            try {
+                System.out.println("登録されている名前は"+findUserById(3)+"さんです。");
+            }catch (NullPointerException e) {
+                System.out.println("登録されていません");
+            }
+        }
+        public static String findUserById(int id) throws NullPointerException{
+            String userName = userById.get(id);
+            if(userName == null) {
+                throw new NullPointerException();
+            }
+            return userName;
+        }
+ }
